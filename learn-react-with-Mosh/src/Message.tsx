cdd4c2add059ea2a8322407570afc1dc1b4b61c4
Ray-Leonard <rayleonard726@gmail.com>
2024-09-24 12:38:10 -0400

    initial push with introduction to react components

---
+++ learn-react-with-Mosh/src/Message.tsx
@@ -0,0 +1,31 @@
+
+function Message() { 
+    // JSX: JavaScript XML, gets compiled to JavaScript eventually.
+    // return <h1>Hello World</h1>;
+
+    // TODO: make a toggle button between 2 scenarios. 
+    const name = "";
+    if(name){
+        return <h1>Hello World, {LoopPrintName(name)}</h1>
+    }
+    else{
+        return LoopJSX();
+    }
+}
+
+function LoopPrintName(name : string){
+    let output : string = "";
+    for(let i: number = 0; i < 60; i++){
+        output += name + " ";
+    }
+
+    return output;
+}
+
+function LoopJSX(){
+    const helloWorld = <h1>Hello World</h1>
+    return <>{Array(5).fill(helloWorld)}</>
+}
+
+// make sure other files can access it.
+export default Message;
