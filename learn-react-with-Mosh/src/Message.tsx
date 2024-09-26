c6e04660c543246de3f912024694b3375ef22b2d
Ray-Leonard <rayleonard726@gmail.com>
2024-09-26 01:06:35 -0400

    Getting bootstrap and rendering a listgroup

--- learn-react-with-Mosh/src/Message.tsx
+++ learn-react-with-Mosh/src/Message.tsx
@@ -1,30 +1,28 @@
+function Message() {
+  // JSX: JavaScript XML, gets compiled to JavaScript eventually.
+  // return <h1>Hello World</h1>;
 
-function Message() { 
-    // JSX: JavaScript XML, gets compiled to JavaScript eventually.
-    // return <h1>Hello World</h1>;
-
-    // TODO: make a toggle button between 2 scenarios. 
-    const name = "";
-    if(name){
-        return <h1>Hello World, {LoopPrintName(name)}</h1>
-    }
-    else{
-        return LoopJSX();
-    }
+  // TODO: make a toggle button between 2 scenarios.
+  const name = "R";
+  if (name) {
+    return <h1>Hello World, {LoopPrintName(name)}</h1>;
+  } else {
+    return LoopJSX();
+  }
 }
 
-function LoopPrintName(name : string){
-    let output : string = "";
-    for(let i: number = 0; i < 60; i++){
-        output += name + " ";
-    }
+function LoopPrintName(name: string) {
+  let output: string = "";
+  for (let i: number = 0; i < 60; i++) {
+    output += name + " ";
+  }
 
-    return output;
+  return output;
 }
 
-function LoopJSX(){
-    const helloWorld = <h1>Hello World</h1>
-    return <>{Array(5).fill(helloWorld)}</>
+function LoopJSX() {
+  const helloWorld = <h1>Hello World</h1>;
+  return <>{Array(5).fill(helloWorld)}</>;
 }
 
 // make sure other files can access it.
