3114ad4662c223b9b326bd4cc9ab059223451cf4
Ray-Leonard <rayleonard726@gmail.com>
2024-09-28 23:58:58 -0400

    conditional rendering

--- learn-react-with-Mosh/src/components/ListGroup.tsx
+++ learn-react-with-Mosh/src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 function ListGroup() {
-  const items = [
+  let items = [
     "dog",
     "pig",
     "cat",
@@ -11,16 +11,33 @@ function ListGroup() {
     "list-group-item",
   ];
 
+  items = [];
+
+  // 1. put the conditional expression in a variable
+  const emptyListMessageVar =
+    items.length === 0 ? <p>No item found (emptyListMessageVar).</p> : null;
+
+  // 2. put the expression expression in a function (could benefit from params)
+  const emptyListMessageFunc = () => {
+    return items.length === 0 ? (
+      <p>No item found (emptyListMessageFunc).</p>
+    ) : null;
+  };
+
   return (
     <>
       <h1>My List</h1>
+
+      {emptyListMessageVar}
+      {emptyListMessageFunc()}
+      {items.length === 0 && <p>No item found (logical AND expression)</p>}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
             {item}
           </li>
         ))}
-        ;
       </ul>
     </>
   );
